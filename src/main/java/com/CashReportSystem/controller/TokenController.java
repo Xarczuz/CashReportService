53e9dba4c6fbc0e212325a1165c74b8ec67e3d98
Perham <perham132@hotmail.se>
2019-12-10 16:20:45 +0100

    test

--- src/main/java/com/CashReportSystem/controller/TokenController.java
+++ src/main/java/com/CashReportSystem/controller/TokenController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author Perham
+ */
 @RestController
 @RequestMapping(value = "token")
 public class TokenController {
